9ca4d3aa29061b748adb0d48ee9428e3534e7dae
zaibniaz <zaib_niaz@hotmail.com>
2019-10-10 08:47:58 -0700

    adding code to dir

---
+++ app/src/main/java/com/zaib/coroutine_api_sample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.zaib.coroutine_api_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
